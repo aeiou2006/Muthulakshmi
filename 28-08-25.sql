5c34b66bb13c34ebd62f0aa41c30c6f501ff94a7
aeiou2006 <muthulakshmi19906@gmail.com>
2025-08-28 08:05:25 -0800

    Add files via upload

---
+++ 28-08-25.sql
@@ -0,0 +1,114 @@
+drop DATABASE ORG123;
+SHOW DATABASES;
+USE ORG123;
+
+CREATE TABLE Worker (
+	WORKER_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	FIRST_NAME CHAR(25),
+	LAST_NAME CHAR(25),
+	SALARY INT(15),
+	JOINING_DATE DATETIME,
+	DEPARTMENT CHAR(25)
+);
+
+INSERT INTO Worker 
+	(WORKER_ID, FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT) VALUES
+		(001, 'Monika', 'Arora', 100000, '14-02-20 09.00.00', 'HR'),
+		(002, 'Niharika', 'Verma', 80000, '14-06-11 09.00.00', 'Admin'),
+		(003, 'Vishal', 'Singhal', 300000, '14-02-20 09.00.00', 'HR'),
+		(004, 'Amitabh', 'Singh', 500000, '14-02-20 09.00.00', 'Admin'),
+		(005, 'Vivek', 'Bhati', 500000, '14-06-11 09.00.00', 'Admin'),
+		(006, 'Vipul', 'Diwan', 200000, '14-06-11 09.00.00', 'Account'),
+		(007, 'Satish', 'Kumar', 75000, '14-01-20 09.00.00', 'Account'),
+		(008, 'Geetika', 'Chauhan', 90000, '14-04-11 09.00.00', 'Admin');
+select * from worker;
+
+select * from worker where department = 'admin' order by salary asc;
+
+select department ,count(department)
+from worker
+group by department order by department asc;
+
+select department,sum(salary)
+from worker
+group by department order by salary asc;
+
+select department,count(salary)
+from worker
+where department not in('hr','admin')
+group by department;
+
+select department,max(salary)
+from worker
+group by department;
+
+select department,min(salary)
+from worker
+where department not in('account')
+group by department;
+
+select first_name from worker  order by  first_name asc;
+select salary from worker order by salary desc;
+
+SELECT * FROM Worker
+ORDER BY WORKER_ID
+limit 5 OFFSET 5 ;
+
+SELECT * FROM (
+SELECT *, ROW_NUMBER() OVER (ORDER BY WORKER_ID) AS rn
+FROM Worker
+) AS temp
+WHERE rn>5;
+ 
+select first_name, LTRIM(first_name) from worker;
+select first_name, RTRIM(first_name) from worker;
+
+SELECT FIRST_NAME, UPPER(FIRST_NAME) FROM Worker;
+SELECT FIRST_NAME, LOWER(FIRST_NAME) FROM Worker;
+
+select department,avg(salary)from worker group by department having avg(salary)>100000;
+select department,max(salary)from worker group by department having max(salary)=500000;
+select department,count(department),sum(salary) from worker group by department having count(department)>=1 and sum(salary)>100000;
+select department ,avg(salary)from worker group by department having avg(salary) between 50000 and 200000;
+create database orgdb;
+use orgdb;
+create table award_winners(
+id int,
+name varchar(25),
+award_name varchar(30));
+
+insert into award_winners values(001,'Karthick','Volleyball winners'),(002,'Gopi','Basketball Runners'),(003,'Adib','Oscar winners'),
+(004,'Harij','English SpellBee 2nd Runner-up'),(005,'Loki','Volleyball winners'),(006,'Sakthi','Badminton Nationals runners'),
+(007,'Iqhlas','Debug winners'),(008,'AK','Basketball runners'),(009,'Kamal','Hackathon runners'),
+(010,'Barath','Hackathon winners');
+
+select * from award_winners;
+
+create table students(
+id int,
+name varchar(25));
+
+insert into students values(005,'Loki'),(1001,'Srihari'),(1003,'Dinesh'),(001,'Karthick'),(1005,'Tharun'),
+(1006,'Vamsi'),(1007,'Monish'),(1008,'Rohith'),(1009,'Alwyn'),(1010,'Akash');
+
+select * from students;
+
+select * from students 
+where id=(select id from award_winners where id=5);
+
+select * from students 
+where id=(select id from award_winners where name="karthick");
+
+select * from award_winners
+where id=(select id from students where id=1);
+
+select * from award_winners
+where id=(select id from students where name="Loki");
+
+
+ 
+
+
+
+
+
